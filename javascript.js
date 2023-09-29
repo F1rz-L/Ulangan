9cc064c2d3101e6c2bcfa567de82729e374aa785
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-09-29 09:44:03 +0700

    Persiapan

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// alert("Javascript");
+
+// btn.onlick = function(){
+//     const suhu = document.querySelector("#suhu");
+//     const konversi = document.querySelector("#hasil");
+
+//     let hasil = (9/5 * suhu.value) + 32;
+
+//     konversi.innerHTML=hasil;
+// }
+
+
+// btn.onclick=function(){
+//     const meter = document.querySelector("#jarak");
+//     const konversi = document.querySelector("#hasil");
+//     let hasil = meter.value**3;
+
+//     konversi.innerHTML=hasil;
+// }
+
+
+// btn.onclick = function(){
+//     document.querySelector("h2").style.fontSize="2px";
+// }
+
+
+// btn.onclick=function(){
+//     const div = document.querySelector("div");
+//     const para = document.createElement("p");
+
+//     para.textContent="belajar js";
+//     div.appendChild(para);
+// }
+
+btn.onclick=function(){
+    const div = document.querySelector("div");
+    const h = document.createElement("h1");
+
+    h.textContent="Lorem ipsum dolor sit amet adipiscum liberta";
+    div.appendChild(h);
+
+}
